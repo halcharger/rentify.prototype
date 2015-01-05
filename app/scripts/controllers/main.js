5670783d66453c6a28412fdd13c3309ee026fb60
halcharger <allen.firth@gmail.com>
2015-01-05 11:44:56 +0200

    initial commit of yo angular template

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rentifyprototypeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rentifyprototypeApp
+ */
+angular.module('rentifyprototypeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
